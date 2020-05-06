2fa7e5268218768e1eb58d2ca93ed3dd10533a7b
Yuxin Li <yuxinli915@outlook.com>
2020-05-06 13:15:01 -0500

    gets movie trend

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
-const genres = [];
-class Genre {
-  constructor(genreID) {
-    this.genre = genreID;
-    this.movieList = [];
-  }
+fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=48cb631a848d6fe61082677d12aca120&language=en-US`)
+  .then(data => {
+    if (data.ok) {
+      return data.json();
+    } else {
+      throw new Error(`Fail to get data.`);
+    }
+  })
+  .then(genreList => {
+    getMovieList(genreList);
+  })
+
+function getMovieList(list) {
+  fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=48cb631a848d6fe61082677d12aca120`)
+    .then(data => {
+      if (data.ok) {
+        return data.json();
+      } else {
+        throw new Error(`Fail to get data.`);
+      }
+    })
+    .then(movieList => {
+      
+    })
 }
